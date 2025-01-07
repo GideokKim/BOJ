80d5c0942cd76ee8a7b71eee1f19345a194aaf1b
gideok Kim <ikid4127@naver.com>
2025-01-07 16:20:03 +0900

    feat: implement BOJ 6190 with tests

---
+++ 6000/src/6190_another_cow_number_game.h
@@ -0,0 +1,40 @@
+#ifndef BOJ_6000_SRC_6190_ANOTHER_COW_NUMBER_GAME_H_
+#define BOJ_6000_SRC_6190_ANOTHER_COW_NUMBER_GAME_H_
+
+#include <iostream>
+
+class AnotherCowNumberGame {
+ public:
+  void SetInputs() { std::cin >> number; }
+
+  void Calculate() {
+    score = 0;
+    while (number != 1) {
+      if (number & 1) {
+        number = number + ((number + 1) >> 1);
+        score += 2;
+      } else {
+        number = number >> 1;
+        score += 1;
+      }
+    }
+  }
+
+  void PrintScore() { std::cout << score << "\n"; }
+
+ private:
+  int number;
+  int score;
+};
+
+#ifdef BOJ_SUBMIT
+int main() {
+  AnotherCowNumberGame another_cow_number_game;
+  another_cow_number_game.SetInputs();
+  another_cow_number_game.Calculate();
+  another_cow_number_game.PrintScore();
+  return 0;
+}
+#endif  // BOJ_SUBMIT
+
+#endif  // BOJ_6000_SRC_6190_ANOTHER_COW_NUMBER_GAME_H_
