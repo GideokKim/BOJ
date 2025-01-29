b4ac565fedd1ab81e5516c3b692384b7db54a0d2
gideok Kim <ikid4127@naver.com>
2025-01-30 08:54:21 +0900

    test(17000): remove boiler plate code

--- 17000/tests/17224_why_did_apc_become_subtask_competition_test.cc
+++ 17000/tests/17224_why_did_apc_become_subtask_competition_test.cc
@@ -1,78 +1,18 @@
 #include "17000/src/17224_why_did_apc_become_subtask_competition.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(WhyDidAPCBecomeSubtaskCompetitionTest, SimpleCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
+class WhyDidAPCBecomeSubtaskCompetitionTest
+    : public IOTestFixture<WhyDidAPCBecomeSubtaskCompetition> {};
 
-  std::string input = "4 8 4\n1 8\n4 5\n6 20\n9 12";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  WhyDidAPCBecomeSubtaskCompetition why_did_apc_become_subtask_competition;
-  why_did_apc_become_subtask_competition.SetInputs();
-  why_did_apc_become_subtask_competition.Calculate();
-  why_did_apc_become_subtask_competition.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "380";
-
-  EXPECT_EQ(oss.str(), expected_output);
+TEST_F(WhyDidAPCBecomeSubtaskCompetitionTest, SimpleCase) {
+  RunTest("4 8 4\n1 8\n4 5\n6 20\n9 12", "380");
 }
 
-TEST(WhyDidAPCBecomeSubtaskCompetitionTest, SimpleCase2) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
-
-  std::string input = "8 7 5\n1 3\n2 5\n3 5\n4 8\n5 8\n6 9\n7 10";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  WhyDidAPCBecomeSubtaskCompetition why_did_apc_become_subtask_competition;
-  why_did_apc_become_subtask_competition.SetInputs();
-  why_did_apc_become_subtask_competition.Calculate();
-  why_did_apc_become_subtask_competition.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "660";
-
-  EXPECT_EQ(oss.str(), expected_output);
+TEST_F(WhyDidAPCBecomeSubtaskCompetitionTest, SimpleCase2) {
+  RunTest("8 7 5\n1 3\n2 5\n3 5\n4 8\n5 8\n6 9\n7 10", "660");
 }
 
-TEST(WhyDidAPCBecomeSubtaskCompetitionTest, SimpleCase3) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
-
-  std::string input = "8 9 5\n1 8\n3 10\n4 5\n5 20\n7 12\n8 15\n9 50\n14 14";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  WhyDidAPCBecomeSubtaskCompetition why_did_apc_become_subtask_competition;
-  why_did_apc_become_subtask_competition.SetInputs();
-  why_did_apc_become_subtask_competition.Calculate();
-  why_did_apc_become_subtask_competition.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "580";
-
-  EXPECT_EQ(oss.str(), expected_output);
+TEST_F(WhyDidAPCBecomeSubtaskCompetitionTest, SimpleCase3) {
+  RunTest("8 9 5\n1 8\n3 10\n4 5\n5 20\n7 12\n8 15\n9 50\n14 14", "580");
 }
--- 17000/tests/17608_bar_test.cc
+++ 17000/tests/17608_bar_test.cc
@@ -1,53 +1,9 @@
 #include "17000/src/17608_bar.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(BarTest, SimpleCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
+class BarTest : public IOTestFixture<Bar> {};
 
-  std::string input = "6\n6\n9\n7\n6\n4\n6";
+TEST_F(BarTest, SimpleCase) { RunTest("6\n6\n9\n7\n6\n4\n6", "3"); }
 
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  Bar bar;
-  bar.SetInputs();
-  bar.Calculate();
-  bar.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "3";
-
-  EXPECT_EQ(oss.str(), expected_output);
-}
-
-TEST(BarTest, SimpleCase2) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
-
-  std::string input = "5\n5\n4\n3\n2\n1";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  Bar bar;
-  bar.SetInputs();
-  bar.Calculate();
-  bar.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "5";
-
-  EXPECT_EQ(oss.str(), expected_output);
-}
+TEST_F(BarTest, SimpleCase2) { RunTest("5\n5\n4\n3\n2\n1", "5"); }
