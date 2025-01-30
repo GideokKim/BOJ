a8a71759d572574994ae614b2c25c8f7d05af949
gideok Kim <ikid4127@naver.com>
2025-01-30 19:41:04 +0900

    test(21000): remove boiler plate codes

--- 21000/tests/21955_split_test.cc
+++ 21000/tests/21955_split_test.cc
@@ -1,51 +1,9 @@
 #include "21000/src/21955_split.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(SplitTest, SampleCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
+class SplitTest : public IOTestFixture<Split> {};
 
-  std::string input = "2341";
+TEST_F(SplitTest, SampleCase) { RunTest("2341", "23 41"); }
 
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  Split split;
-  split.SetInputs();
-  split.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "23 41";
-
-  EXPECT_EQ(oss.str(), expected_output);
-}
-
-TEST(SplitTest, SampleCase2) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
-
-  std::string input = "238445";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  Split split;
-  split.SetInputs();
-  split.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "238 445";
-
-  EXPECT_EQ(oss.str(), expected_output);
-}
+TEST_F(SplitTest, SampleCase2) { RunTest("238445", "238 445"); }
