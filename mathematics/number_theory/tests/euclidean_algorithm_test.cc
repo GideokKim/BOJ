3900f9ad576100659f2c90695daeb947ac9bc833
gideok Kim <ikid4127@naver.com>
2025-01-31 16:25:59 +0900

    test(number theory): add test cases for euclidean algorithm

--- mathematics/number_theory/tests/euclidean_algorithm_test.cc
+++ mathematics/number_theory/tests/euclidean_algorithm_test.cc
@@ -10,4 +10,8 @@ TEST_F(EuclideanAlgorithmTest, SampleCase) { RunTest("10 12", "2"); }
 
 TEST_F(EuclideanAlgorithmTest, SampleCase2) { RunTest("2484 4212", "108"); }
 
+TEST_F(EuclideanAlgorithmTest, SampleCase3) { RunTest("111 111111", "111"); }
+
+TEST_F(EuclideanAlgorithmTest, SampleCase4) { RunTest("111 1111111", "1"); }
+
 }  // namespace boj::mathematics::number_theory
