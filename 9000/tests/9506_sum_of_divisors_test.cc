f2b9ce52108245236f781d7887527cc2ab5a5142
gideok Kim <ikid4127@naver.com>
2025-02-02 08:07:03 +0900

    test(9000): remove boiler plate codes

--- 9000/tests/9506_sum_of_divisors_test.cc
+++ 9000/tests/9506_sum_of_divisors_test.cc
@@ -1,27 +1,10 @@
 #include "9000/src/9506_sum_of_divisors.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(SumOfDivisorsTest, SampleCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
+class SumOfDivisorsTest : public IOTestFixture<SumOfDivisors> {};
 
-  std::string input = "6\n12\n28\n-1\n";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  SumOfDivisors sum_of_divisors;
-  sum_of_divisors.Calculate();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output =
-      "6 = 1 + 2 + 3\n12 is NOT perfect.\n28 = 1 + 2 + 4 + 7 + 14\n";
-
-  EXPECT_EQ(oss.str(), expected_output);
+TEST_F(SumOfDivisorsTest, SampleCase) {
+  RunTest("6\n12\n28\n-1\n",
+          "6 = 1 + 2 + 3\n12 is NOT perfect.\n28 = 1 + 2 + 4 + 7 + 14\n");
 }
