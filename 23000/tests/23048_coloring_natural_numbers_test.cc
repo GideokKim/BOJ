f0b1a20c0c7797852118d9c5d8361017b0c74cf2
gideok Kim <ikid4127@naver.com>
2025-02-04 08:27:14 +0900

    test(23000): remove boiler plate codes

--- 23000/tests/23048_coloring_natural_numbers_test.cc
+++ 23000/tests/23048_coloring_natural_numbers_test.cc
@@ -1,23 +1,10 @@
 #include "23000/src/23048_coloring_natural_numbers.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(ColoringNaturalNumbersTest, SampleCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
+class ColoringNaturalNumbersTest
+    : public IOTestFixture<ColoringNaturalNumbers> {};
 
-  std::string input = "5";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  ColoringNaturalNumbers coloring_natural_numbers;
-  coloring_natural_numbers.SetInputs();
-  coloring_natural_numbers.GeneratePrimeNumbers();
-  coloring_natural_numbers.SetColors();
-  int result = coloring_natural_numbers.GetK();
-  std::vector<unsigned long> colors = coloring_natural_numbers.GetColors();
-  std::cin.rdbuf(orig_cin);
-
-  EXPECT_EQ(result, 4);
-  EXPECT_EQ(colors, std::vector<unsigned long>({1, 2, 3, 2, 4}));
+TEST_F(ColoringNaturalNumbersTest, SampleCase) {
+  RunTest("5", "4\n1 2 3 2 4 ");
 }
