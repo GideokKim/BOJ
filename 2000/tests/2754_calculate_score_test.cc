0644dad0c49b5b25c14eb92428b7d38e6bd6521f
gideok Kim <ikid4127@naver.com>
2025-02-19 09:12:49 +0900

    feat: implement BOJ 2754 with test

---
+++ 2000/tests/2754_calculate_score_test.cc
@@ -0,0 +1,6 @@
+#include "2000/src/2754_calculate_score.h"
+#include "test_headers/test_helper.h"
+
+class CalculateScoreTest : public IOTestFixture<CalculateScore> {};
+
+TEST_F(CalculateScoreTest, SampleCase) { RunTest("A0", "4.0"); }
