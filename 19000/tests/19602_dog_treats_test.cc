b84a3f47eeae31c52fcafe654932acb37ddf502e
gideok Kim <ikid4127@naver.com>
2025-03-04 12:07:17 +0900

    test(19000): remove boiler plate codes

--- 19000/tests/19602_dog_treats_test.cc
+++ 19000/tests/19602_dog_treats_test.cc
@@ -1,53 +1,9 @@
 #include "19000/src/19602_dog_treats.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(DogTreatsTest, SimpleCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
+class DogTreatsTest : public IOTestFixture<DogTreats> {};
 
-  std::string input = "3\n1\n0";
+TEST_F(DogTreatsTest, SimpleCase) { RunTest("3\n1\n0", "sad"); }
 
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  DogTreats dog_treats;
-  dog_treats.SetInputs();
-  dog_treats.Calculate();
-  dog_treats.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "sad";
-
-  EXPECT_EQ(oss.str(), expected_output);
-}
-
-TEST(DogTreatsTest, SimpleCase2) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
-
-  std::string input = "3\n2\n1";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  DogTreats dog_treats;
-  dog_treats.SetInputs();
-  dog_treats.Calculate();
-  dog_treats.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "happy";
-
-  EXPECT_EQ(oss.str(), expected_output);
-}
+TEST_F(DogTreatsTest, SimpleCase2) { RunTest("3\n2\n1", "happy"); }
