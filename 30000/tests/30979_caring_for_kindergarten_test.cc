89d638bfb335905485f2a6a18808159cc560f210
gideok Kim <ikid4127@naver.com>
2025-03-04 11:30:02 +0900

    test(30000): remove boiler plate codes

--- 30000/tests/30979_caring_for_kindergarten_test.cc
+++ 30000/tests/30979_caring_for_kindergarten_test.cc
@@ -1,37 +1,14 @@
 #include "30000/src/30979_caring_for_kindergarten.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(CaringForKindergartenTest, HappyCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
+class CaringForKindergartenTest : public IOTestFixture<CaringForKindergarten> {
+};
 
-  std::string input = "5\n2\n2 3";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  CaringForKindergarten caring_for_kindergarten;
-  caring_for_kindergarten.Calculate();
-  std::string result = caring_for_kindergarten.GetResult();
-
-  std::cin.rdbuf(orig_cin);
-
-  EXPECT_EQ(result, "Padaeng_i Happy");
+TEST_F(CaringForKindergartenTest, HappyCase) {
+  RunTest("5\n2\n2 3", "Padaeng_i Happy");
 }
 
-TEST(CaringForKindergartenTest, CryCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-
-  std::string input = "6\n2\n2 3";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  CaringForKindergarten caring_for_kindergarten;
-  caring_for_kindergarten.Calculate();
-  std::string result = caring_for_kindergarten.GetResult();
-
-  std::cin.rdbuf(orig_cin);
-
-  EXPECT_EQ(result, "Padaeng_i Cry");
+TEST_F(CaringForKindergartenTest, CryCase) {
+  RunTest("6\n2\n2 3", "Padaeng_i Cry");
 }
--- 30000/tests/30987_haru_dermatologist_test.cc
+++ 30000/tests/30987_haru_dermatologist_test.cc
@@ -1,53 +1,11 @@
 #include "30000/src/30987_haru_dermatologist.h"
 
-#include <gtest/gtest.h>
+#include "test_headers/test_helper.h"
 
-TEST(HaruDermatologistTest, SimpleCase) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
+class HaruDermatologistTest : public IOTestFixture<HaruDermatologist> {};
 
-  std::string input = "0 1\n3 4 2 8 3";
+TEST_F(HaruDermatologistTest, SimpleCase) { RunTest("0 1\n3 4 2 8 3", "-2"); }
 
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  HaruDermatologist haru_dermatologist;
-  haru_dermatologist.SetInputs();
-  haru_dermatologist.Calculate();
-  haru_dermatologist.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "-2";
-
-  EXPECT_EQ(oss.str(), expected_output);
-}
-
-TEST(HaruDermatologistTest, SimpleCase2) {
-  std::streambuf* orig_cin = std::cin.rdbuf();
-  std::streambuf* orig_cout = std::cout.rdbuf();
-
-  std::string input = "-3 3\n-15 -4 5 -2 -5";
-
-  std::istringstream iss(input);
-  std::cin.rdbuf(iss.rdbuf());
-
-  std::ostringstream oss;
-  std::cout.rdbuf(oss.rdbuf());
-
-  HaruDermatologist haru_dermatologist;
-  haru_dermatologist.SetInputs();
-  haru_dermatologist.Calculate();
-  haru_dermatologist.PrintResult();
-
-  std::cin.rdbuf(orig_cin);
-  std::cout.rdbuf(orig_cout);
-
-  std::string expected_output = "-210";
-
-  EXPECT_EQ(oss.str(), expected_output);
+TEST_F(HaruDermatologistTest, SimpleCase2) {
+  RunTest("-3 3\n-15 -4 5 -2 -5", "-210");
 }
