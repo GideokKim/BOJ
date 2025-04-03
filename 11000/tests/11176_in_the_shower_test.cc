4e3aa44926ecce1df4f3319cafab92556c287cba
gideok Kim <ikid4127@naver.com>
2025-04-03 20:10:20 +0900

    feat: implement BOJ 11176 with test

---
+++ 11000/tests/11176_in_the_shower_test.cc
@@ -0,0 +1,15 @@
+#include "11000/src/11176_in_the_shower.h"
+
+#include "test_headers/test_helper.h"
+
+class InTheShowerTest : public IOTestFixture<InTheShower> {};
+
+TEST_F(InTheShowerTest, SampleCase) {
+  RunTest(
+      "1\n"
+      "5 3\n"
+      "1\n"
+      "5\n"
+      "6",
+      "1");
+}
