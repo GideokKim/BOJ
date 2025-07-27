beb9b7d44a268a27f5d1d26be4a2a4735cfce77e
gideok Kim <ikid4127@naver.com>
2025-07-27 15:41:31 +0900

    feat: implement BOJ 24263 with test

---
+++ 24000/tests/24263_algorithm_class_time_2_test.cc
@@ -0,0 +1,7 @@
+#include "24000/src/24263_algorithm_class_time_2.h"
+
+#include "test_headers/test_helper.h"
+
+class AlgorithmClassTime2Test : public IOTestFixture<AlgorithmClassTime2> {};
+
+TEST_F(AlgorithmClassTime2Test, SampleCase) { RunTest("7", "7\n1"); }
