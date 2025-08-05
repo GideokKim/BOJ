b1cd90fefb4b9ebb2fa01e761cd12f1d962bf71c
gideok Kim <ikid4127@naver.com>
2025-08-05 10:06:53 +0900

    feat: implement BOJ 24264 with test

---
+++ 24000/tests/24264_algorithm_class_time_3_test.cc
@@ -0,0 +1,7 @@
+#include "24000/src/24264_algorithm_class_time_3.h"
+
+#include "test_headers/test_helper.h"
+
+class AlgorithmClassTime3Test : public IOTestFixture<AlgorithmClassTime3> {};
+
+TEST_F(AlgorithmClassTime3Test, SampleCase) { RunTest("7", "49\n2"); }
