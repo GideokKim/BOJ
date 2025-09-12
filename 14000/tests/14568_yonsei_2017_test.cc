0fc45a4c931b5c638a46e5ae5812fe034ffcca27
gideok Kim <ikid4127@naver.com>
2025-09-12 22:20:39 +0900

    feat: implement BOJ 14568 with test

---
+++ 14000/tests/14568_yonsei_2017_test.cc
@@ -0,0 +1,7 @@
+#include "14000/src/14568_yonsei_2017.h"
+
+#include "test_headers/test_helper.h"
+
+class Yonsei2017Test : public IOTestFixture<Yonsei2017> {};
+
+TEST_F(Yonsei2017Test, SampleCase) { RunTest("6", "1"); }
