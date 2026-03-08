9f2b8ecfab0911dd021caf94179c05ffeb0bab10
gideok Kim <ikid4127@naver.com>
2026-03-08 20:14:47 +0900

    feat: impl boj 34759 with test

---
+++ 34000/tests/34759_zebra_and_lion_test.cc
@@ -0,0 +1,7 @@
+#include "34000/src/34759_zebra_and_lion.h"
+
+#include "test_headers/test_helper.h"
+
+class ZebraAndLionTest : public IOTestFixture<ZebraAndLion> {};
+
+TEST_F(ZebraAndLionTest, SampleCase) { RunTest("3", "6"); }
