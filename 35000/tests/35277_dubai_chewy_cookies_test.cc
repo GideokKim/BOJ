300fe427a07d32b6b4d2d5bf708fbda6ea8400a4
gideok Kim <ikid4127@naver.com>
2026-03-10 22:11:49 +0900

    feat: impl boj 35277 with test

---
+++ 35000/tests/35277_dubai_chewy_cookies_test.cc
@@ -0,0 +1,7 @@
+#include "35000/src/35277_dubai_chewy_cookies.h"
+
+#include "test_headers/test_helper.h"
+
+class DubaiChewyCookiesTest : public IOTestFixture<DubaiChewyCookies> {};
+
+TEST_F(DubaiChewyCookiesTest, SampleCase) { RunTest("2026", "1"); }
